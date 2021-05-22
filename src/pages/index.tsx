82de4633e34cff53c147387458a1c6470b35715d
Gerhard <gerhard@linumlabs.com>
2021-05-22 21:49:50 +0200

    add animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import { AppContext } from 'next/app';
 import Head from 'next/head'
+import { Linum } from '../components/Icons';
 import WalletModal from '../components/WalletModal/WalletModal'
 import { useWallets } from '../store/walletContext/WalletContext';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   const { showWalletModal } = useWallets();
+
+
   return (
     <div className={styles.container}>
       <Head >
@@ -14,9 +17,11 @@ export default function Home() {
         <link key="favicon" rel="icon" href="/linum_labs.svg" color="#F1F4F8" />
       </Head>
       {showWalletModal && <WalletModal />}
+      <Linum style={{ width: "10rem", height: "10rem", position: "absolute", left: "calc(50vw - 5rem)", top: "calc(50vh - 5rem)", animation: "rotate 10s infinite linear" }} />
     </div>
   )
 }
+
 
 export async function getServerSideProps(appContext: AppContext) {
   return {
