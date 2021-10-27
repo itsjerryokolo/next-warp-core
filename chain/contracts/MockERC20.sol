3e5f78b3fca08d3dc6dd7bd3e490028763cf37c9
wschwab <w.s.schwab4@gmail.com>
2021-10-27 19:17:06 +0300

    added OZ ERC20 permit; started on 721 permit

--- chain/contracts/MockERC20.sol
+++ chain/contracts/MockERC20.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract MockERC20 is ERC20 {
-  constructor() ERC20("Testing Token", "TEST") {}
+contract MockERC20 is ERC20, ERC20Permit {
+  constructor() ERC20("Testing Token", "TEST") ERC20Permit("TestingToken") {}
 
   function mint(address to, uint256 amount) public {
     _mint(to, amount);
